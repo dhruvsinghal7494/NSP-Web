36f00671347874ebc3477c9a91f3c76bf208c767
Dhruv Singhal <dhruvsinghal1973@gmail.com>
2024-01-11 11:37:58 +0530

    center

---
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -0,0 +1,129 @@
+import Image from "next/image";
+import MenuLink from "./menuLink/menuLink";
+import styles from "./sidebar.module.css";
+import {
+  MdDashboard,
+  MdSupervisedUserCircle,
+  MdShoppingBag,
+  MdAttachMoney,
+  MdWork,
+  MdAnalytics,
+  MdOutlineAttachMoney,
+  MdOutlineSettings,
+  MdHelpCenter,
+  MdPeople,
+  MdLogout,
+} from "react-icons/md";
+import { auth, signOut } from "@/app/auth";
+
+const menuItems = [
+  {
+    title: "Pages",
+    list: [
+      {
+        title: "Dashboard",
+        path: "/dashboard",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Users",
+        path: "/dashboard/users",
+        icon: <MdSupervisedUserCircle />,
+      },
+      {
+        title: "Products",
+        path: "/dashboard/products",
+        icon: <MdShoppingBag />,
+      },
+      {
+        title: "Transactions",
+        path: "/dashboard/transactions",
+        icon: <MdAttachMoney />,
+      },
+    ],
+  },
+  {
+    title: "Analytics",
+    list: [
+      {
+        title: "Arihanta",
+        path: "/dashboard/arihanta",
+        icon: <MdWork />,
+      },
+      {
+        title: "Donation",
+        path: "/dashboard/donation",
+        icon: <MdAnalytics />,
+      },
+      {
+        title: "Namaskar",
+        path: "/dashboard/namaskar",
+        icon: <MdPeople />,
+      },
+      {
+        title: "Vishesh Kripa",
+        path: "/dashboard/visheshkripa",
+        icon: <MdOutlineAttachMoney />,
+      },
+    ],
+  },
+  {
+    title: "User",
+    list: [
+      {
+        title: "Settings",
+        path: "/dashboard/settings",
+        icon: <MdOutlineSettings />,
+      },
+      {
+        title: "Help",
+        path: "/dashboard/help",
+        icon: <MdHelpCenter />,
+      },
+    ],
+  },
+];
+
+const Sidebar = async () => {
+  const { user } = await auth();
+  return (
+    <div className={styles.container}>
+      <div className={styles.user}>
+        <Image
+          className={styles.userImage}
+          src={user.img || "/noavatar.png"}
+          alt=""
+          width="50"
+          height="50"
+        />
+        <div className={styles.userDetail}>
+          <span className={styles.username}>{user.username}</span>
+          <span className={styles.userTitle}>Administrator</span>
+        </div>
+      </div>
+      <ul className={styles.list}>
+        {menuItems.map((cat) => (
+          <li key={cat.title}>
+            <span className={styles.cat}>{cat.title}</span>
+            {cat.list.map((item) => (
+              <MenuLink item={item} key={item.title} />
+            ))}
+          </li>
+        ))}
+      </ul>
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <button className={styles.logout}>
+          <MdLogout />
+          Logout
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ app/ui/login/loginForm/loginForm.jsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { authenticate } from "@/app/lib/actions";
+import styles from "./loginForm.module.css";
+import { useFormState } from "react-dom";
+// import Image from "next/image";
+
+const LoginForm = () => {
+  const [state, formAction] = useFormState(authenticate, undefined);
+
+  return (
+    <div className={styles.page}>
+      {/* <Image
+        src="/Guruji.webp"
+        alt="Mahabrahmrishi"
+        className={styles.logo}
+        width={400}
+        height={400}
+      ></Image> */}
+      <div className="">
+        <form action={formAction} className={styles.form}>
+          <h1>Login</h1>
+          <p>Username</p>
+          <input type="text" placeholder="username" name="username" />
+          <p>Password</p>
+          <input type="password" placeholder="password" name="password" />
+          <button>Submit</button>
+          {state && state}
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
